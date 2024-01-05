aeefc2377f85d3b3ef81d850d261acdf4e41146b
Nipun Liyanage <51255650+NipunLiyanage@users.noreply.github.com>
2024-01-05 10:43:03 +0530

    Update 4_💻_API Test Script Gen.py

--- pages/4_💻_API Test Script Gen.py
+++ pages/4_💻_API Test Script Gen.py
@@ -45,7 +45,7 @@ if 'nonMandatoryRequestPayloadParameters' in st.session_state:
     nonMandatoryRequestpayloadPar = st.session_state.nonMandatoryRequestPayloadParameters
 
 if 'response' in st.session_state:
-    tcResponse = st.session_state.response.content
+    tcResponse = st.session_state.response
 
 
 
