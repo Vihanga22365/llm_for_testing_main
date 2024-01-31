7cb506857615382866b541e5fdfdf8f506d05f22
Nipun Liyanage <51255650+NipunLiyanage@users.noreply.github.com>
2024-01-31 09:13:13 +0530

    Changes in the UI test script gen backend and UI

--- pages/7_💻_UI Test Script Gen.py
+++ pages/7_💻_UI Test Script Gen.py
@@ -47,8 +47,21 @@ else:
 
 st.write('Please fill the details below with respect to the test case you want to be generated')
 
+# template = """ 
+# I want to generate selenium test script for below test case. Below I mentioned the test case and business requirement.\n
+# Test case type - {testCaseType}\n
+# Test case - {testCase}\n
+# Business Requirement:\n
+# User Story Name - {userStoryName}\n
+# Main Business Functionality - {mainBusinessFunc}\n
+# Sub Business Functionalites - {subBusinessFunc}\n
+# Precondition - {precondition}\n
+# Type of End Users - {endUsersType}\n
+# Think you are a QA engineer. You need to mainly consider above mention test case and generate selenium script for {language} according to that testcase, as a professional QA engineer. When you write selenium script please follow coding best practices, coding standards, exception handling as a QA engineer.
+# """
+
 template = """ 
-I want to generate selenium test script for below test case. Below I mentioned the test case and business requirement.\n
+I want to generate selenium test script for below test case. Below I mentioned the test case,business requirement and instructions.\n
 Test case type - {testCaseType}\n
 Test case - {testCase}\n
 Business Requirement:\n
@@ -56,9 +69,69 @@ User Story Name - {userStoryName}\n
 Main Business Functionality - {mainBusinessFunc}\n
 Sub Business Functionalites - {subBusinessFunc}\n
 Precondition - {precondition}\n
-Type of End Users - {endUsersType}\n
-Think you are a QA engineer. You need to mainly consider above mention test case and generate selenium script for {language} according to that testcase, as a professional QA engineer. When you write selenium script please follow coding best practices, coding standards, exception handling as a QA engineer.
+Additional Information - {additionalInfo}
+Already available functions : - {reusableFunc},(please reuse these where applicable and these are the only existing functions.)\n
+Instructions - Develop a Selenium script in {language} to automate the positive test case described above.
+Adhere to coding best practices, coding standards, and implement robust exception handling.
+Use the web driver as defined.
+Consider parameterization for flexibility and adaptability.
+Implement detailed logging for debugging purposes.
+Utilize a testing framework such as TestNG or JUnit.
+Follow the Page Object Model for improved maintainability.
+Include waits effectively using explicit waits.
+Don't be limited to the already available functions.
+When generating a new function other than the ones mentioned, assume the existence of appropriate UI components and always provide the code in the body, 
+You are not required to write the code in the body only for the already implemented functions. 
+If any already existing functions are reused, indicate where they are used.
+
+
+
 """
+
+
+# template = """ 
+# I want to generate selenium test script for below test case. Below I mentioned the test case and business requirement.\n
+# Test case type - {testCaseType}\n
+# Test case - {testCase}\n
+# Business Requirement:\n
+# User Story Name - {userStoryName}\n
+# Main Business Functionality - {mainBusinessFunc}\n
+# Sub Business Functionalites - {subBusinessFunc}\n
+# Precondition - {precondition}\n
+# Type of End Users - {endUsersType}\n
+# Additional Information - {additionalInfo}
+# Already available functions (please use if applicable and these are only the existing functions): : {reusableFunc}n
+# Instructions:  Develop a Selenium script in {language} to automate the positive test case described above.
+# Adhere to coding best practices, coding standards, and implement robust exception handling.
+# Use the web driver as defined.
+# {instructions}
+# Include waits effectively using explicit waits.
+# Not required to write already implemented functions.
+# Do not keep functions with only comments, assume the existence of appropriate UI components and implement the code as much as you can.
+# If any existing functions are reused, indicate where they are used.
+
+
+
+# """
+
+additional_info_text: str = """
+Element identification methods: XPaths\n
+Programming language to use: Java with Selenium\n
+Web driver to use: Chrome
+"""
+
+reusable_text: str = """
+login(driver, username, password), goToScheduledMeetingPage(driver), removeSomeoneFromTheMeeting(driver, userId)
+
+"""
+
+instruction_text: str = """
+Consider parameterization for flexibility and adaptability.
+Implement detailed logging for debugging purposes.
+Utilize a testing framework such as TestNG or JUnit.
+Follow the Page Object Model for improved maintainability
+"""
+
 on = st.toggle('Populate fields with a sample scenario')
 if on:
     with st.form('api_ts_gen'):
@@ -70,7 +143,10 @@ if on:
         # st.text_input('Test Scenario Combination', placeholder='', key = 'testScenarioCombination')
         st.text_input('Precondition', placeholder='Precondition', value='The meeting request should come from the XYZ end. ',key = 'precondition', help="Please Enter the Pre Conditions that should be met.")
         st.text_input('Type of End Users', placeholder='Type of End Users',value='Two types of agents named Relationship Managers and Financial Advisors', key = 'endUsersType', help="Enter the type of the End Users as per their roles.")
-        st.selectbox('Select the Language',('Python', 'Java'),key = 'language',placeholder='Select for which language the selenium script should be generated',index=None)
+        st.text_area('Additional Information',placeholder='Element Identification methods, Web drivers to be used etc.',value='Element identification methods: XPaths\nWeb driver to use: Chrome', key='additionalInfo', help='Information such as Element Identification Methods Ex: XPath, Web Drivers Ex: Chrome etc.' )
+        st.text_area('Reusable Functions', placeholder='Functions that are readily available to be reused',key ='reusableFunc',value='login(driver, username, password), goToScheduledMeetingPage(driver), removeUserFromTheMeeting(driver, userId)' ,help='Please type thed definitions of the readily available functions that should be reused in the script.',height=75)
+        #st.text_area('Instructions', placeholder='Any additional instructions to the model to be followed when generating the script',value='Consider parameterization for flexibility and adaptability.\nImplement detailed logging for debugging purposes.\nUtilize a testing framework such as TestNG or JUnit.\nFollow the Page Object Model for improved maintainability.', key='instructions',help='Please add any instructions that the LLM should follow when generating the script.',height=150)
+        st.selectbox('Select the Language',('Python', 'Java'),key = 'language',placeholder='Select for which language the selenium script should be generated',index=1)
         submitted = st.form_submit_button("Generate")
 
 else: 
@@ -83,12 +159,15 @@ else:
         # st.text_input('Test Scenario Combination', placeholder='', key = 'testScenarioCombination')
         st.text_input('Precondition', placeholder='Precondition', key = 'precondition', help="Please Enter the Pre Conditions that should be met.")
         st.text_input('Type of End Users', placeholder='Type of End Users', key = 'endUsersType', help="Enter the type of the End Users as per their roles.")
+        st.text_area('Additional Information',placeholder='Element Identification methods, Web drivers to be used etc.',key='additionalInfo', help='Information such as Element Identification Methods Ex: XPath, Web Drivers Ex: Chrome etc.' )
+        st.text_area('Reusable Functions', placeholder='Functions that are readily available to be reused',key ='reusableFunc',help='Please type thed definitions of the readily available functions that should be reused in the script.')
+        #st.text_area('Instructions', placeholder='Any additional instructions to the model to be followed when generating the script', key='instructions',help='Please add any instructions that the LLM should follow when generating the script.')
         st.selectbox('Select the Language',('Python', 'Java'),key = 'language',placeholder='Select for which language the selenium script should be generated',index=None)
         submitted = st.form_submit_button("Generate")
 
 if submitted: 
     ui_ts_template = PromptTemplate.from_template(template)
-    ui_ts_template.input_variables = ['testCaseType','testCase','userStoryName','mainBusinessFunc','subBusinessFunc','precondition','endUsersType','language']
+    ui_ts_template.input_variables = ['testCaseType','testCase','userStoryName','mainBusinessFunc','subBusinessFunc','precondition','language','additionalInfo','reusableFunc']
 
     formatted_prompt = ui_ts_template.format(
         testCaseType = st.session_state.testCaseType,
@@ -98,8 +177,12 @@ if submitted:
         subBusinessFunc = st.session_state.subBusinessFunc,
         # testScenarioCombination = st.session_state.testScenarioCombination,
         precondition = st.session_state.precondition,
-        endUsersType = st.session_state.endUsersType,
-        language = st.session_state.language
+        #endUsersType = st.session_state.endUsersType,
+        language = st.session_state.language,
+        additionalInfo = st.session_state.additionalInfo,
+        reusableFunc = st.session_state.reusableFunc,
+        #instructions = st.session_state.instructions
+
     )
 
 
@@ -107,7 +190,7 @@ if submitted:
 
         st.write('Using: '+model)
 
-        llm = OpenAI(model_name= "gpt-3.5-turbo-0613", temperature = 0.5)
+        llm = OpenAI(model_name= "gpt-3.5-turbo-0613", temperature = 0.1)
 
         if(len(formatted_prompt) != 0):
             response = llm(formatted_prompt)
